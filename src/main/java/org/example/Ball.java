edf0de7c879ebe91416bea22ef39d6708a8bccc5
Mikhail Markov <mikhaiil@mmarkovv.ru>
2023-09-05 10:54:15 +0300

    Реализованы кострукторы и приватные поля

--- src/main/java/org/example/Ball.java
+++ src/main/java/org/example/Ball.java
@@ -1,5 +1,14 @@
 package org.example;
 
 public class Ball {
+    private double x = 0.0;
 
+    private double y = 0.0;
+
+    public Ball() {}
+
+    public Ball(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 }

5900c82cb17640de6dcf519960a50aa1531842a6
Mikhail Markov <mikhaiil@mmarkovv.ru>
2023-09-05 10:59:28 +0300

    Implemented rest of Ball fucntionality

--- src/main/java/org/example/Ball.java
+++ src/main/java/org/example/Ball.java
@@ -11,4 +11,21 @@ public class Ball {
         this.x = x;
         this.y = y;
     }
+
+    public double getX() { return x; }
+
+    public void setX(double x) { this.x = x; }
+
+    public double getY() { return y; }
+
+    public void setY(double y) { this.y = y; }
+
+    public void setXY(double x, double y) { this.x = x; this.y = y; }
+
+    public void move(double xDisp, double yDisp) { x += xDisp; y += yDisp; }
+
+    @Override
+    public String toString() {
+        return String.format("(%.2f, %.2f)", x, y);
+    }
 }

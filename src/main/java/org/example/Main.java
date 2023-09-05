1deb9383bf3ec0c77ca9e22e72e8a00a31050b8a
Mikhail Markov <mikhaiil@mmarkovv.ru>
2023-09-05 10:40:58 +0300

    Implemented class Author. Created class Ball

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static long factorialRecursive(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
 
+    public static long factorial(int n) {
+        long res = 1;
+        while (n > 0) {
+            res *= n;
+            --n;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.printf("%d %d", factorialRecursive(n), factorial(n));
     }
 }

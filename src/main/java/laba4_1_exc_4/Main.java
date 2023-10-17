2d302dafcfa9517dc3a9cb24e265e4609541a767
Mikhail Markov <mikhaiil@mmarkovv.ru>
2023-10-17 09:24:11 +0300

    Implemented Matrix

---
+++ src/main/java/laba4_1_exc_4/Main.java
@@ -0,0 +1,31 @@
+package laba4_1_exc_4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Matrix a = new Matrix(), b = new Matrix();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ввод матрицы a\n" +
+                           "--------------");
+        a.inputMatrix();
+        System.out.println("Ввод матрицы b\n" +
+                "--------------");
+        b.inputMatrix();
+
+        System.out.println("a + b");
+        Matrix res = a.sum(b);
+        res.show();
+
+        int k;
+        System.out.print("Введите целое число k, на которое надо умножить матрицу: ");
+        k = sc.nextInt();
+        System.out.println("a *= k");
+        a.multiplyByValue(k);
+        a.show();
+
+        System.out.println("a * b");
+        a.multiplyByMatrix(b).show();
+    }
+}
--- src/main/java/laba4_1_exc_4/Matrix.java
+++ src/main/java/laba4_1_exc_4/Matrix.java
@@ -1,12 +1,47 @@
 package laba4_1_exc_4;
 
+import laba4_1_exc_2.Phone;
+
+import java.util.Scanner;
+
 public class Matrix {
     private double[][] data;
-    private long rows;
-    private long cols;
+    private int rows;
+    private int cols;
+
+    public Matrix() {
+        rows = 0;
+        cols = 0;
+    }
+
+    public Matrix(int rows, int cols) {
+        data = new double[rows][cols];
+        this.rows = rows;
+        this.cols = cols;
+    }
+
+    public void inputMatrix() {
+        int rows, cols;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите количество строк: ");
+        rows = sc.nextInt();
+
+        System.out.print("Введите количество столбцов: ");
+        cols = sc.nextInt();
+
+        data = new double[rows][cols];
+        this.rows = rows;
+        this.cols = cols;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                data[i][j] = sc.nextInt();
+            }
+        }
+    }
 
     public Matrix sum(Matrix other) {
-        Matrix res = new Matrix();
+        Matrix res = new Matrix(rows, cols);
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
                 res.data[i][j] = data[i][j] + other.data[i][j];
@@ -16,11 +51,31 @@ public class Matrix {
     }
 
     public void multiplyByValue(int value) {
-        Matrix res = new Matrix();
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
                 data[i][j] *= value;
             }
         }
+    }
+
+    public void show() {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.print('\n');
+        }
+    }
+
+    public Matrix multiplyByMatrix(Matrix other) {
+        Matrix res = new Matrix(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                for (int k = 0; k < cols; ++k) {
+                    res.data[i][j] += data[i][k] * other.data[k][j];
+                }
+            }
+        }
+        return res;
     }
 }

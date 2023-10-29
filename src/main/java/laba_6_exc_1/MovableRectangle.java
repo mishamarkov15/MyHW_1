318dc7b0ab3dd3ac90ae1ff9ad232be63051f8ee
Mikhail Markov <mikhaiil@mmarkovv.ru>
2023-10-29 21:05:57 +0300

    Done task 2 laba 6

---
+++ src/main/java/laba_6_exc_1/MovableRectangle.java
@@ -0,0 +1,42 @@
+package laba_6_exc_1;
+
+public class MovableRectangle implements Movable {
+    private MovablePoint topLeft;
+    private MovablePoint bottomRight;
+
+    public MovableRectangle(int x1, int y1, int x2, int y2, int xSpeed, int ySpeed) {
+        topLeft = new MovablePoint(x1, y1, xSpeed, ySpeed);
+        bottomRight = new MovablePoint(x2, y2, xSpeed, ySpeed);
+    }
+
+    @Override
+    public String toString() {
+        return "Top left:\n" +
+                "---------\n" + topLeft.toString() + "Right bottom:\n" +
+                "-------------\n" + bottomRight.toString();
+    }
+
+    @Override
+    public void moveUp() {
+        topLeft.moveUp();
+        bottomRight.moveUp();
+    }
+
+    @Override
+    public void moveDown() {
+        topLeft.moveDown();
+        bottomRight.moveDown();
+    }
+
+    @Override
+    public void moveLeft() {
+        topLeft.moveLeft();
+        bottomRight.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        topLeft.moveRight();
+        bottomRight.moveRight();
+    }
+}

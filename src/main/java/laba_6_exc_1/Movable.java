19588b4762bbfcb74a51c18279f7755939d80d78
Mikhail Markov <mikhaiil@mmarkovv.ru>
2023-10-29 21:01:15 +0300

    Done task 1 laba 6

---
+++ src/main/java/laba_6_exc_1/Movable.java
@@ -0,0 +1,11 @@
+package laba_6_exc_1;
+
+public interface Movable {
+    public void moveUp();
+
+    public void moveDown();
+
+    public void moveLeft();
+
+    public void moveRight();
+}
---
+++ src/main/java/laba_6_exc_1/MovablePoint.java
@@ -0,0 +1,40 @@
+package laba_6_exc_1;
+
+public class MovablePoint implements Movable {
+    protected int x;
+    protected int y;
+    protected int xSpeed;
+    protected int ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "Point: (%d, %d), speed: (%d, %d)\n".formatted(x, y, xSpeed, ySpeed);
+    }
+
+    @Override
+    public void moveUp() {
+        y += ySpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        y -= ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        x -= xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        x += xSpeed;
+    }
+}

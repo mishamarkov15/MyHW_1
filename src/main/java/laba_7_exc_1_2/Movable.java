eb81776691df4c93f344bbd8a72aaa99680aab2e
Mikhail Markov <mikhaiil@mmarkovv.ru>
2023-10-31 09:55:16 +0300

    Done laba 7 task 1-2

---
+++ src/main/java/laba_7_exc_1_2/Movable.java
@@ -0,0 +1,11 @@
+package laba_7_exc_1_2;
+
+public interface Movable {
+    public void moveUp();
+
+    public void moveDown();
+
+    public void moveLeft();
+
+    public void moveRight();
+}
---
+++ src/main/java/laba_7_exc_1_2/MovableCircle.java
@@ -0,0 +1,36 @@
+package laba_7_exc_1_2;
+
+public class MovableCircle implements Movable {
+    private int radius;
+    private MovablePoint center;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return center.toString() + "Radius: %d\n".formatted(radius);
+    }
+
+    @Override
+    public void moveUp() {
+        center.moveUp();
+    }
+
+    @Override
+    public void moveDown() {
+        center.moveDown();
+    }
+
+    @Override
+    public void moveLeft() {
+        center.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        center.moveRight();
+    }
+}

5978f8dab7e5e2c449f5340f78c2fd260f84eb80
Mikhail Markov <mikhaiil@mmarkovv.ru>
2023-12-03 17:22:46 +0300

    Done laba 13 exc 3

---
+++ src/main/java/laba_13_exc3/Address.java
@@ -0,0 +1,55 @@
+package laba_13_exc3;
+
+import java.util.StringTokenizer;
+
+public class Address {
+    private String country;
+    private String region;
+    private String city;
+    private String street;
+    private String house;
+    private String building; // Корпус
+    private String flat; // Квартира
+
+    public Address() {
+    }
+
+    public void parseAddress(String src) {
+        var words = src.split(", ");
+        for (int i = 0; i < words.length; ++i) {
+            switch (i) {
+                case 0 -> country = words[i];
+                case 1 -> region = words[i];
+                case 2 -> city = words[i];
+                case 3 -> street = words[i];
+                case 4 -> house = words[i];
+                case 5 -> building = words[i];
+                case 6 -> flat = words[i];
+            }
+        }
+    }
+
+    public void parseAddress(String src, String delimeter) {
+        var tokenizer = new StringTokenizer(src, delimeter);
+        int i = 0;
+        while (tokenizer.hasMoreTokens()) {
+            switch (i) {
+                case 0 -> country = tokenizer.nextToken();
+                case 1 -> region = tokenizer.nextToken();
+                case 2 -> city = tokenizer.nextToken();
+                case 3 -> street = tokenizer.nextToken();
+                case 4 -> house = tokenizer.nextToken();
+                case 5 -> building = tokenizer.nextToken();
+                case 6 -> flat = tokenizer.nextToken();
+                default -> tokenizer.nextToken();
+            }
+
+            ++i;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Страна: %s\nРегион: %s\nГород: %s\nУлица: %s\nДом: %s\nСтроение: %s\nКвартира: %s".formatted(country, region, city, street, house, building, flat);
+    }
+}
---
+++ src/main/java/laba_13_exc3/Main.java
@@ -0,0 +1,19 @@
+package laba_13_exc3;
+
+public class Main {
+    public static void main(String[] args) {
+        Address a = new Address();
+
+        a.parseAddress("Россия, Московский, Москва, Арбат, 1, 1, 77");
+        System.out.printf("Тест 1:\n-------\n%s\n", a);
+
+        a.parseAddress("Россия. Ленинградский. Санкт-Петербург. Ленина. 2. 3. 12", ". ");
+        System.out.printf("Тест 2:\n-------\n%s\n", a);
+
+        a.parseAddress("Германия; Центральный; Берлин; Фон Бисмарка; 9; 2; 9", "; ");
+        System.out.printf("Тест 3:\n-------\n%s\n", a);
+
+        a.parseAddress("Россия, Самарский, Самара, Площадь революции, 65, 1, 1");
+        System.out.printf("Тест 4:\n-------\n%s\n", a);
+    }
+}
